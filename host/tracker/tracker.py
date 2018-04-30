a8796abf5580a7ed700f84e58125984ceff96dc3
weedle1912 <weedle1912@gmail.com>
2018-04-30 07:05:12 +0200

    if-else fix

--- host/tracker/tracker.py
+++ host/tracker/tracker.py
@@ -3,8 +3,8 @@ import threading
 import sys
 import time
 
-if cv2.__version__ < '4.3.0':
-    raise ImportError('Please upgrade your OpenCV installation to v4.3.* or later!')
+if cv2.__version__ < '3.4.0':
+    raise ImportError('Please upgrade your OpenCV installation to v3.4.* or later!')
 
 class Tracker:
     def __init__(self, tracker_type='MEDIANFLOW'):
@@ -32,15 +32,15 @@ class Tracker:
 def setTrackerType(tracker_type):
     if tracker_type == 'BOOSTING':
         tracker = cv2.TrackerBoosting_create()
-    else if tracker_type == 'MIL':
+    elif tracker_type == 'MIL':
         tracker = cv2.TrackerMIL_create()
-    else if tracker_type == 'KCF':
+    elif tracker_type == 'KCF':
         tracker = cv2.TrackerKCF_create()
-    else if tracker_type == 'TLD':
+    elif tracker_type == 'TLD':
         tracker = cv2.TrackerTLD_create()
-    else if tracker_type == 'MEDIANFLOW':
+    elif tracker_type == 'MEDIANFLOW':
         tracker = cv2.TrackerMedianFlow_create()
-    else if tracker_type == 'GOTURN':
+    elif tracker_type == 'GOTURN':
         tracker = cv2.TrackerGOTURN_create()
     else:
         tracker = None
