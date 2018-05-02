1f16844ea6fa883bcd7a2fa94833809ba8dc68c4
Unknown <weedle1912@gmail.com>
2018-05-02 14:46:51 +0200

    Update test function in main

--- host/tracker/main.py
+++ host/tracker/main.py
@@ -1,34 +1,34 @@
 import cv2
 from tracker import Tracker
-from framehandler import FrameHandler
 
-VIDEO_PATH = "../../videos/BlueAngels.mp4"
+VIDEO_PATH = "../../videos/HobbyKing.mp4"
 
 def main():
-    handler = FrameHandler(VIDEO_PATH)
+    cap = cv2.VideoCapture(VIDEO_PATH)
     tracker = Tracker()
-
-    handler.start()
-    handler.wait_new_frame()
-    ok, frame = handler.read()
-    tracker.init(frame, (60, 60, 130, 90))
+    isInit = False
+    bbox = (474,469,143,62)
 
     while True:
-        handler.wait_new_frame()
-        ok, frame = handler.read()
+        ok, frame = cap.read()
         if not ok:
             break
-        tracker.update(frame)
+        
+        if not isInit:
+            tracker.init(frame, bbox)
+            isInit = True
+        else:
+            tracker.update(frame)
+        
         bbox = tracker.get_bbox()
 
-        # Draw bbox
         draw_bbox(frame, bbox)
         cv2.imshow('frame', frame)
         if ( cv2.waitKey(1) & 0xFF == ord('q') ):
             break
     
-    handler.stop()
-    cv2.destroyAlleWindows()
+    cap.release()
+    cv2.destroyAllWindows()
 
 def draw_bbox(frame, bbox):
     cv2.rectangle(frame,(bbox[0],bbox[1]),(bbox[0]+bbox[2],bbox[1]+bbox[3]),(0,255,0),2)
