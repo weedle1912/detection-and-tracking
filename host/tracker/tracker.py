62b4f2bfb3678da1a841ef63b4faad9b00a1271f
Unknown <weedle1912@gmail.com>
2018-05-02 14:47:26 +0200

    Update init call function

--- host/tracker/tracker.py
+++ host/tracker/tracker.py
@@ -9,20 +9,24 @@ if cv2.__version__ < '3.4.0':
 class Tracker:
     def __init__(self, tracker_type='MEDIANFLOW'):
         self.tracker_type = tracker_type
-        self.tracker = setTrackerType(self.tracker_type)
         self.bbox = (0,0,0,0)
         self.read_lock = threading.Lock()
 
     def init(self, frame, bbox):
+        self.tracker = setTrackerType(self.tracker_type)
         ok = self.tracker.init(frame, bbox)
+        with self.read_lock:
+            self.bbox = bbox
         return ok
 
     def clear(self):
         self.tracker = setTrackerType(self.tracker_type)
     
     def update(self, frame):
+        ok, bbox = self.tracker.update(frame)
+        bbox = [int(x) for x in bbox]
         with self.read_lock:
-            ok, self.bbox = self.tracker.update(frame)
+            self.bbox = bbox
     
     def get_bbox(self):
         with self.read_lock:
