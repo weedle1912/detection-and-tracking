59e7e6fa90437936f9f86ee560b6142582ca5e5b
Unknown <weedle1912@gmail.com>
2018-05-16 17:30:53 +0200

    Update script to handle multiple files as input

--- host/scripts/show_gt.py
+++ host/scripts/show_gt.py
@@ -2,28 +2,39 @@ import cv2
 import time
 import argparse
 
+BGR = [(0,0,255), (0,255,0), (255,0,0)]
+
 def main(args):
     cap = cv2.VideoCapture(args['input'])
-    f = open(args['file'], 'r')
     width, height = args['size'][0], args['size'][1]
-    bboxes = []
-    for line in f:
-        x, y, w, h = line.strip().split(',')
-        x = int(float(x)*width)
-        y = int(float(y)*height)
-        w = int(float(w)*width)
-        h = int(float(h)*height)
-        bboxes.append((x,y,w,h))
-    f.close()
-    print('Count: %d frames'%len(bboxes))
+    series = []
+    for i in range(len(args['file'])):
+        bboxes = []
+        f = open(args['file'][i], 'r')
+        for line in f:
+            line = line.strip()
+            if not line == '()':
+                x, y, w, h = line.strip().split(',')
+                bbox = scale((float(x),float(y),float(w),float(h)), width, height)
+            else:
+                bbox = ()
+            bboxes.append(bbox)
+        f.close()
+        series.append(bboxes)
+    
+    n = len(series[0])
+    for s in series:
+        n = min(n, len(s))
 
-    for bbox in bboxes:
+    for i in range(n):
         ok, frame = cap.read()
         if not ok: 
             break
         frame = cv2.resize(frame, (args['size'][0], args['size'][1]), interpolation=cv2.INTER_AREA)
-        if bbox:
-            cv2.rectangle(frame,(bbox[0],bbox[1]),(bbox[0]+bbox[2],bbox[1]+bbox[3]),(0,255,0),1)
+        for j in range(len(series)):
+            bbox = series[j][i]
+            draw_bbox(frame, bbox, j)    
+        
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) == 27: # Exit with 'esc' key
             break
@@ -32,12 +43,25 @@ def main(args):
     cap.release()
     cv2.destroyAllWindows()
 
+def scale(bbox, width, height):
+    x = int(bbox[0]*width)
+    y = int(bbox[1]*height)
+    w = int(bbox[2]*width)
+    h = int(bbox[3]*height)
+    return (x,y,w,h)
+
+def draw_bbox(frame, bbox, color):
+    if color > 2:
+        color = 2
+    if bbox:
+        cv2.rectangle(frame,(bbox[0],bbox[1]),(bbox[0]+bbox[2],bbox[1]+bbox[3]),BGR[color],2)
+
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument('-i', '--input', required=True,
         help='path to input video')
-    ap.add_argument('-f', '--file', required=True,
-        help='input gt file')
+    ap.add_argument('-f', '--file', required=True, nargs='+',
+        help='normalized csv bbox file(s)')
     ap.add_argument('-s', '--size', nargs=2, type=int, default=[640, 480],
         metavar=('WIDTH', 'HEIGHT'),
         help='frame size')
