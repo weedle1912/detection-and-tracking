7c3e9bfa849bbbd315d508308fd62c2a3626940d
Unknown <weedle1912@gmail.com>
2018-05-22 18:56:11 +0200

    Renamed script 'show_gt', added frame count

--- host/scripts/show_annotations.py
+++ host/scripts/show_annotations.py
@@ -2,7 +2,7 @@ import cv2
 import time
 import argparse
 
-BGR = [(0,0,255), (0,255,0), (255,0,0)]
+BGR = [(255,0,0), (0,0,255), (0,255,0)]
 
 def main(args):
     cap = cv2.VideoCapture(args['input'])
@@ -26,17 +26,24 @@ def main(args):
     for s in series:
         n = min(n, len(s))
 
+    if args['play']:
+        val = 1
+    else:
+        val = 0
+
     for i in range(n):
         ok, frame = cap.read()
         if not ok: 
             break
         frame = cv2.resize(frame, (args['size'][0], args['size'][1]), interpolation=cv2.INTER_AREA)
+        #cv2.rectangle(img,(xb,yb),(xb+wb,yb+hb),BGR[color],-1) 
+        cv2.putText(frame,'Frame: %04d'%(i+1),(10,20), cv2.FONT_HERSHEY_PLAIN, 1,(255,255,255),1,cv2.LINE_AA)
         for j in range(len(series)):
             bbox = series[j][i]
             draw_bbox(frame, bbox, j)    
         
         cv2.imshow('frame', frame)
-        if cv2.waitKey(1) == 27: # Exit with 'esc' key
+        if cv2.waitKey(val) == 27: # Exit with 'esc' key
             break
         time.sleep(0.03)
 
@@ -65,6 +72,9 @@ if __name__ == '__main__':
     ap.add_argument('-s', '--size', nargs=2, type=int, default=[640, 480],
         metavar=('WIDTH', 'HEIGHT'),
         help='frame size')
+    ap.add_argument('-p', '--play', action='store_true',
+        help='play video instead of stepping through each frame')
     args = vars(ap.parse_args())   
 
     main(args)
+    
